10a90d51fb6aae3141a2298cd7f98a134f028f63
hanan320 <Hanoona3-20@hotmail.com>
2024-05-19 20:20:24 +0000

    edit images

--- travel-destination/src/App.js
+++ travel-destination/src/App.js
@@ -1,7 +1,7 @@
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Home from './components/home/Home.js'
-import TourDetails from './components/tourDetails/TourDetails.js';
+import TourDetails from './components/tourDetails/TourDetails';
 
 
 function App() {
